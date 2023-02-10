18fc2debeed7010bd2faea5c4f0e33420546c3b5
julientournier <56165771+julientournier6@users.noreply.github.com>
2023-02-10 12:03:41 +0100

    Initial commit

---
+++ src/java/com/isep/seance1.java
@@ -0,0 +1,16 @@
+package com.isep;
+
+import java.lang.reflect.Method;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    public String getName(){
+        AbstractStringBuilder name;
+        if(name.length() < 2){
+            throw new IllegalAccessException();
+        }
+        this.name = name;
+    }
+}
