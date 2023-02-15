3b1b795f5d574de4c0dd6ea545a19ddd99eefd69
julientournier <56165771+julientournier6@users.noreply.github.com>
2023-02-15 22:37:05 +0100

    Création TP1
    Exo somme fini

--- TP1/src/java/com/TP1/question1/somme.java
+++ TP1/src/java/com/TP1/question1/somme.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class somme {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir le premier entier");
         int premierEntier = scanner.nextInt();
         System.out.println("Veuillez saisir le deuxieme entier");
         int deuxiemeEntier = scanner.nextInt();
         int somme = premierEntier + deuxiemeEntier;
         System.out.println("La somme de " + premierEntier + " avec " + deuxiemeEntier + "est égale à " + somme);
-        System.out.println("Veuillez saisir le premier entier");
 
     }
 }
