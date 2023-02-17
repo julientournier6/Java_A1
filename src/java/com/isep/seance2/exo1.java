03840b68eb2e0aa548088e7c798fc434dadd1f46
julientournier <56165771+julientournier6@users.noreply.github.com>
2023-02-17 09:54:06 +0100

    séance 2
    Exo somme d'un tableau true or false terminé

--- src/java/com/isep/seance2/exo1.java
+++ src/java/com/isep/seance2/exo1.java
@@ -1,15 +1,15 @@
 package com.isep.seance2;
 
 public class exo1 {
-    static boolean isBinaryArraySum(int []T, int val){
+    static boolean isBinaryArraySum(int[] T, int val) {
         int n = T.length;
         int count = 0;
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int subarraySum = 0;
-            for(int j=i; j<n; j++){
+            for (int j = i; j < n; j++) {
                 subarraySum += T[j];
-                if(val == subarraySum){
+                if (val == subarraySum) {
                     count++;
                 }
             }
@@ -17,12 +17,12 @@ public class exo1 {
         return count >= 1;
     }
 
-    public static void main(String[] args)
-    {
-        int val=7;
-        int [] T={1,4,5};
+    public static void main(String[] args) {
+        int val = 7;
+        int[] T = {1, 4, 5};
 
         boolean resultatSomme = isBinaryArraySum(T, val);
         System.out.printf("%d est la somme de deux entiers dans le tableau : %b%n", val, resultatSomme);
     }
+
 }
